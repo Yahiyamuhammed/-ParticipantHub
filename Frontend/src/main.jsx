7c6eee3e134b9ca1ef0112b8b02498a1d37e11e6
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-06-30 12:47:22 +0530

    upt: updated main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import router from "@/app/router";
+
+import { AuthProvider } from "@/context/AuthContext";
+
 import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
