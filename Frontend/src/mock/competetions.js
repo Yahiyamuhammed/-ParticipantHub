763dbe1a728a8c56eb9550d5d267ce3158f2ede5
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2026-06-30 12:43:37 +0530

    feat: add mock datas

---
+++ Frontend/src/mock/competetions.js
@@ -0,0 +1,35 @@
+const competitions = [
+  {
+    id: 1,
+
+    title: "Mapila Song",
+
+    stage: "Stage 4",
+
+    startTime: "2026-07-03T09:30:00",
+
+    endTime: "2026-07-03T10:00:00",
+
+    latitude: 10.998,
+
+    longitude: 76.005,
+  },
+
+  {
+    id: 2,
+
+    title: "Essay",
+
+    stage: "Stage 2",
+
+    startTime: "2026-07-03T11:00:00",
+
+    endTime: "2026-07-03T12:00:00",
+
+    latitude: 10.991,
+
+    longitude: 76.004,
+  },
+];
+
+export default competitions;
---
+++ Frontend/src/mock/participants.js
@@ -0,0 +1,15 @@
+const participant = {
+  id: 1,
+
+  name: "Yahiya Muhammed",
+
+  registrationNumber: "SSF00125",
+
+  district: "Malappuram",
+
+  category: "Senior",
+
+  photo: null,
+};
+
+export default participant;
